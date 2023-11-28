eb0960ce46a7050bfbaa4e6fbae6c5e0ad0d8422
bnutz1 <95152119+bnutz1@users.noreply.github.com>
2023-11-28 12:45:29 -0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Baycan Azeri';
+    : 'Simple Home Improvements';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Intellectual growth should only commence at birth and cease only at death. -Albert Einstein';
